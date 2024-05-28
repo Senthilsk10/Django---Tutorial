e42b6be5401bce34f2f7e8b57fee86451fa4ddd9
Senthilsk10 <legendsenthilsk10@gmail.com>
2024-05-28 21:51:11 +0530

    For Loop & comments

--- sample/mysite/views.py
+++ sample/mysite/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 def initial(request):
-    name = request.GET.get("name")
-    return render(request,"main.html",{"name":name})
+    items = ["carrot","beetroot","potato"]
+    dict_items = {
+        "name":"senthil",
+        "age":20
+    }
+    return render(request,"main.html",{"items":items,"d_items":dict_items})
